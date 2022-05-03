3d50c1c59548a72a860b23e0605165e291810b30
Kimvtq <phuongkim1502@gmail.com>
2022-05-03 20:37:11 +0700

    Competed code with adding the function of reading SKU data from an xlsx file
    * Reading SKU data from an xlsx file
    * Loop the data and download images for each SKU.
    * Media folder store all images.

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# Zalora_assignment
+## Zalora_assignment
+
+# Question
+The Business Excellence team would like to build an automated solution to automate a manual
+scraping process in the Commercial department. The team has given you this task, and they
+would like to see you demonstrate your abilities by automating the scraping of product media
+(images and videos) for ZALORA SKUs on the ZALORA website.
+The task for Question 1 is as follows:
+● Write a script in Python to scrape all product media (images and video) for the SKUs
+listed down in this file
+● You are required to send us your script and the product media (result)
+● The script must allow for all the SKU product media to be easily obtained for all the SKUs
+without having to manually access or open the ZALORA website
+● All media must be placed into different subfolders of SKUs, i.e. each subfolder must have
+images and videos for only 1 SKU.
+● For example, for this SKU (A3B30AAC6FF98CGS), based on the SKU’s product display
+view (PDV), there are 4 product images and 1 video associated with this item. The script
+must ensure that the 4 images and 1 video are automatically scraped (downloaded) into
+a folder labelled “A3B30AAC6FF98CGS” in your PC, before scraping the product media
+for the next SKU in the list.
+● Please submit the code on a git repo, try to follow best practices as possible from writing
+good commit messages, clean code, testing, having scalability in mind as well, provide
+your suggested architecture and why you chose each component.
+
+# To create the app
+
+1. Download ChromeDriver and add to the project folder
+ https://chromedriver.storage.googleapis.com/index.html?path=101.0.4951.41/
+ 
+2. Put the file sku_list.xlsx to the project folder
+ 
+2. Install libraries
+	import requests: to scrape website content
+	import os: to create new folders and direct 
+	from selenium import webdriver: simulate user actions
+	from selenium.webdriver.common.by import By: handle find elements in Selenium
+	import openpyxl: to read data from .xlsx file
+	
+3. Step in code
+	-	Read data from local excel file
+	-	For each SKU, direct to the detailed page
+	-	Get all the images and download to local folders
+	
+
+# To execute
+Run the command: py .\main.py
--- main.py
+++ main.py
@@ -2,46 +2,77 @@ import requests
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import openpyxl
+import time
 
 # define a method to create a new folder and direct to it
-def create_a_folder(folder_name):
+def create_then_go_to_folder(folder_name):
+    folder_dir = os.path.join(os.getcwd(), folder_name)
     try:
-        os.mkdir(os.path.join(os.getcwd(), folder_name))
+        os.mkdir(folder_dir)
     except:
         pass
-    os.chdir(os.path.join(os.getcwd(), folder_name))
+    os.chdir(folder_dir)
 
 # define a method to get to detailed page of a SKU then download all images
 def download_image(sku):
     # Direct to search result page for a sku
     driver.get("https://www.zalora.com.my/catalog/?q=" + sku)
 
-    # Get the result and click on the link
-    result_link = driver.find_element(By.XPATH,'//li[@id="' + sku + '"]//a')
-    result_link.click()
+    
+    try:
+        # Get the result and click on the link
+        result_link = driver.find_element(By.XPATH,'//li[@id="' + sku + '"]//a')
+        result_link.click()
 
-    # Get current page / sku detailed page
-    url = driver.current_url
-    print(url)
+        # Get all thumbnails except video 
+        thumbnails  = driver.find_elements(By.XPATH,'//ul[contains(@class,"prd-moreImagesList")]//li[not(div)]')
 
-    # Get all thumbnails except video 
-    thumbnails  = driver.find_elements(By.XPATH,'//ul[contains(@class,"prd-moreImagesList")]//li[not(div)]')
-
-    for thumbmail in thumbnails:
-        thumbmail.click()
-        img = driver.find_element(By.CSS_SELECTOR,'.prd-image')
-        name = img.get_attribute("alt")
-        link = img.get_attribute('src')
-        with open(name.replace(' ', '-').replace('/', '') + '.jpg', 'wb') as f:
-            im = requests.get(link)
-            f.write(im.content)
+        for thumbmail in thumbnails:
+            # thumbmail.click()
+        
+            # img = driver.find_element(By.CSS_SELECTOR,'.prd-image')
+            name = thumbmail.get_attribute("data-image-alt")
+            link = thumbmail.get_attribute('data-image-big')
+            with open(name.replace(' ', '-').replace('/', '') + '.jpg', 'wb') as f:
+                im = requests.get(link)
+                f.write(im.content)
+    except:
+        pass
 
 # Initial driver
 driver = webdriver.Chrome("ChromeDriver\chromedriver.exe")   
-create_a_folder("media")
+driver.maximize_window()
 
-create_a_folder("8EFE6SH6649021GS")
-download_image("8EFE6SH6649021GS")
+# Location of the file
+path = "sku_list.xlsx"
 
-     
+# to open the workbook
+# workbook object is created
+wb_obj = openpyxl.load_workbook(path)
+sheet_obj = wb_obj.active
+ 
+max_row = sheet_obj.max_row
+
+create_then_go_to_folder("media")
+media_dir = os.getcwd()
+
+# Loop will print all values
+# of first column
+for i in range(2, max_row + 1):
+    cell_obj = sheet_obj.cell(row = i, column = 1)
+    sku = cell_obj.value
+    if sku != None:
+        print(sku) # to check sku 
+        sku.strip();
+        create_then_go_to_folder(sku)
+        download_image(sku)
+        # go back to media folder
+        os.chdir(media_dir)
+
+
+# time.sleep(10)
+
+
+
 
